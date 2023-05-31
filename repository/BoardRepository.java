1080ed9406d84938cb0a46dcb27e85771015dfd3
tjsdn9803 <leeinjoo2@inu.ac.kr>
2023-05-31 17:24:38 +0900

    Revert "Member 스프링 빈 등록 방식 변경"
    This reverts commit 278f71e903fc02600c45404792167589dd73670d.

--- repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package hello.hellospring.repository;
-
-import hello.hellospring.domain.BoardEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
-}
